34a88682eb8db4ecd2f3e1d7f17afb8e4ba3c5f3
radact8 <rrcf30@gmail.com>
2025-06-04 16:00:55 +0900

    change compkamoku.c

--- report2/compkamoku.c
+++ report2/compkamoku.c
@@ -3,28 +3,10 @@
 int compkamoku(Kamoku *p, Kamoku *q)
 {
     int result;
-    if(p->day > q->day)
+    result = p->day-q->day;
+    if(result == 0)
     {
-        result = 1;
-    }
-    else if (p->day == q->day)
-    {
-        if(p->period > q->period)
-        {
-            result = 1;
-        }
-        else if(p->period == q->period)
-        {
-            result = 0;
-        }
-        else
-        {
-            result = -1;
-        }
-    }
-    else
-    {
-        result = -1;
+        result = p->period-q->period;
     }
     return result;
 }
